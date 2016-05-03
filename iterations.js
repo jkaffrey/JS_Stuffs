cc202a7434d41ee0c4d6f05d2c5fd81e3577c347
Jeremy <jkaffrey@gmail.com>
2016-05-03 15:38:31 -0600

    while loops

--- iterations.js
+++ iterations.js
@@ -16,5 +16,12 @@ console.log(hold);
 var start = 10;
 for (var i = 0; i < start; i++) {
 
-  console.log((i + 1) + " " + (start - i));
+  //console.log((i + 1) + " " + (start - i));
+}
+
+var numberOfLoops = 0;
+while (numberOfLoops < start) {
+
+  console.log(numberOfLoops + 1, start - numberOfLoops);
+  numberOfLoops++;
 }
