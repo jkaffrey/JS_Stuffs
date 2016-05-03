5b1eefa518a6cf8e8b40e873574fbc6afeeeaac8
Jeremy <jkaffrey@gmail.com>
2016-05-03 13:23:20 -0600

    first commit

---
+++ objects.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var personAcross = {
+
+  firstName: "Sam",
+  gender: "male",
+  age: 28,
+  clothing: ["shirt", "pants", "hoodie"]
+}
+
+var personNext = {};
+personNext.firstName = "Jacob";
+personNext.gender = "male";
+personNext.age = 24;
+personNext.clothing = ["shirt", "pants", "hoodie"];
+
+var personDiagonal = {};
+personDiagonal["firstName"] = "Daniel";
+personDiagonal["gender"] = "male";
+personDiagonal["age"] = 31;
+personDiagonal["clothing"] = ["shirt", "shirt2", "pants"];
+
+var tmpName = personAcross.firstName;
+personAcross.firstName = personNext.firstName;
+personNext.firstName = tmpName;
+console.log("Across: " + personAcross.firstName);
+
+tmpName = personNext["firstName"];
+personNext["firstName"] = personDiagonal["firstName"];
+personDiagonal["firstName"] = tmpName;
+console.log("Diagonal: " + personDiagonal["firstName"]);
+
+var user = {
+  username: 'billymayes',
+  email: 'billymayes@here.com',
+  password: 'oxiclean123'
+};
+
+console.log("Password: " + user.password);
+delete user.password;
+console.log("And now: " + user.password);
