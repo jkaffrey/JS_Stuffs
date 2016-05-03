0b0c4d13e72e34055aa310059d30392ff24c73b8
Jeremy <jkaffrey@gmail.com>
2016-05-03 16:01:26 -0600

    More stuff

--- iterations.js
+++ iterations.js
@@ -11,7 +11,7 @@ if (cash >= 200) {
   hold *= .85;
 }
 
-console.log(hold);
+//console.log(hold);
 
 var start = 10;
 for (var i = 0; i < start; i++) {
@@ -22,6 +22,46 @@ for (var i = 0; i < start; i++) {
 var numberOfLoops = 0;
 while (numberOfLoops < start) {
 
-  console.log(numberOfLoops + 1, start - numberOfLoops);
+  if (!(numberOfLoops%2)) {
+    //console.log(numberOfLoops + 1, start - numberOfLoops);
+  }
   numberOfLoops++;
+}
+
+var nameArray = ['Jordan', 'Logan', 'Micah', 'Roger', 'Jeff'];
+for (var i = 0; i < nameArray.length; i++) {
+
+  console.log(nameArray[i]);
+}
+
+console.log("-- WHILE --");
+
+var i = 0;
+while (i < nameArray.length) {
+
+  console.log(nameArray[i]);
+  i++;
+}
+
+var myStr = "My cars are AWESOME!";
+var newStr = "";
+for (var i = (myStr.length - 1); i >= 0; i--) {
+
+  newStr += myStr[i];
+}
+
+console.log(newStr);
+
+var myCars = {
+  'Jordan' : 'Mustang',
+  'Logan' : 'GTO',
+  'Micah' : 'Lancer Evolution',
+  'Roger' : 'Charger',
+  'Jeff' : 'Lamborghini'
+};
+
+var car;
+for (car in myCars) {
+
+  console.log(myCars[car]);
 }
